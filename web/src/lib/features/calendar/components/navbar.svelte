ede2e98621e47994d02f0d3a8c848dd6f70d47d1
Sithu Khant <122719409+sithu-khant@users.noreply.github.com>
2025-05-21 12:42:47 +0630

    5.21.07

--- web/src/lib/features/calendar/components/navbar.svelte
+++ web/src/lib/features/calendar/components/navbar.svelte
@@ -24,43 +24,42 @@
 </script>
 
 <div class="flex justify-between items-center py-3">
-  <div class="flex items-center">
-    <button
-      class="btn btn-sm btn-square mr-2"
-      onclick={() => toggleSidebar(mainSidebarId)}
-    >
+  <div class="flex items-center gap-2">
+    <button class="btn btn-square" onclick={() => toggleSidebar(mainSidebarId)}>
       <PanelRight size="20px" />
     </button>
 
     <p class="text-lg font-semibold">May 21, 2025</p>
-  </div>
 
-  <div class="tabs tabs-sm tabs-box bg-base-300">
-    {#each views as view}
-      <input
-        type="radio"
-        name={view}
-        aria-label={view[0].toUpperCase() + view.slice(1)}
-        onclick={handleCalViewChange}
-        class="tab px-3 text-sm"
-        checked={$calView === view}
-      />
-    {/each}
+    <div>
+      <button class="btn btn-sm btn-square">
+        <ChevronLeft size="20px" />
+      </button>
+
+      <button class="btn btn-sm">Today</button>
+
+      <button class="btn btn-sm btn-square">
+        <ChevronRight size="20px" />
+      </button>
+    </div>
   </div>
 
   <div class="flex items-center gap-1">
-    <button class="btn btn-sm btn-square">
-      <ChevronLeft size="20px" />
-    </button>
-
-    <button class="btn btn-sm">Today</button>
-
-    <button class="btn btn-sm btn-square">
-      <ChevronRight size="20px" />
-    </button>
+    <div class="tabs tabs-sm tabs-box bg-base-300">
+      {#each views as view}
+        <input
+          type="radio"
+          name={view}
+          aria-label={view[0].toUpperCase() + view.slice(1)}
+          onclick={handleCalViewChange}
+          class="tab px-3 text-sm"
+          checked={$calView === view}
+        />
+      {/each}
+    </div>
 
     <button
-      class="ml-2 btn btn-sm btn-square"
+      class="ml-2 btn btn-square"
       onclick={() => toggleSidebar(chatSidebarId)}
     >
       <MessageSquare size="20px" />
