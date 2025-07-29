e7d564bdd965918486c03e73211f6c922f7e2a26
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-07-29 17:19:44 +0630

    7.29.07

--- logs.md
+++ logs.md
@@ -7,6 +7,7 @@
   - (7.29.04) - Updated drizzle config.
   - (7.29.05) - Added zod describe method in schema files.
   - (7.29.06) - Chat list query.
+  - (7.29.07) - Better chat page.
 - Mon Jul 28, 2025
   - (7.28.01) - Extracted vector utils functions.
   - (7.28.02) - Updated vector text fn.
--- src/routes/(main)/chat/[...id]/+page.svelte
+++ src/routes/(main)/chat/[...id]/+page.svelte
@@ -5,37 +5,45 @@
 
   function onsubmit(e: Event) {
     e.preventDefault();
+
     chat.handleSubmit();
   }
 </script>
 
-<main class="flex h-screen flex-col p-5 max-w-[900px] m-auto">
-  <ul class="flex-1 overflow-y-auto p-6 space-y-8">
-    {#each chat.messages as message, i (i)}
-      <li class={`flex ${message.role === "user" ? "justify-end" : "justify-start"}`}>
-        <div
-          class={`${message.role === "user" && "bg-base-300 text-base-content max-w-xs md:max-w-md lg:max-w-lg px-4 py-2 rounded-xl"}`}
-        >
-          {#each message.parts as part, j (j)}
-            {#if part.type === "text"}
-              <div class="whitespace-pre-wrap">{part.text}</div>
-            {/if}
-          {/each}
-        </div>
-      </li>
-    {/each}
+<main class="flex h-screen flex-col">
+  <div class="flex flex-1 flex-col overflow-y-auto">
+    <div class="mx-auto w-full max-w-[900px] flex-1 py-5 px-4 sm:px-6 lg:px-8">
+      <ul class="space-y-8 pb-4">
+        {#each chat.messages as message, i (i)}
+          <li class={`flex ${message.role === "user" ? "justify-end" : "justify-start"}`}>
+            <div
+              class={`${message.role === "user" && "bg-base-300 text-base-content max-w-xs md:max-w-md lg:max-w-lg px-4 py-2 rounded-xl"}`}
+            >
+              {#each message.parts as part, j (j)}
+                {#if part.type === "text"}
+                  <div class="whitespace-pre-wrap">{part.text}</div>
+                {/if}
+              {/each}
+            </div>
+          </li>
+        {/each}
 
-    {#if chat.status === "submitted"}
-      <li class="animate-pulse">Generating...</li>
-    {/if}
-  </ul>
+        {#if chat.status === "submitted"}
+          <li class="animate-pulse">Generating...</li>
+        {/if}
+      </ul>
+    </div>
 
-  <form {onsubmit} class="p-4 flex items-center gap-2">
-    <input
-      bind:value={chat.input}
-      class="input focus:outline-none flex-1"
-      placeholder="Send a message..."
-    />
-    <button type="submit" class="btn btn-primary" disabled={!chat.input.trim()}> Send </button>
-  </form>
+    <form {onsubmit} class="sticky bottom-5 flex w-full items-center gap-2 p-4">
+      <div class="mx-auto w-full max-w-[900px] flex items-center gap-2">
+        <input
+          bind:value={chat.input}
+          class="input input-bordered focus:outline-none flex-1"
+          placeholder="Send a message..."
+        />
+
+        <button type="submit" class="btn btn-primary" disabled={!chat.input.trim()}> Send </button>
+      </div>
+    </form>
+  </div>
 </main>
