8b30a27b973aeeaaf4264733f1fb5cfc94d5516f
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-08-03 14:00:59 +0630

    refactor(config): update commintlint config file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,29 +1,14 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [
-      2,
-      "always",
-      [
-        "db",
-        "ui",
-        "auth",
-        "api",
-        "types",
-        "constants",
-        "chat",
-        "calendar",
-        "config",
-        "deps", // for dependency updates
-        "release" // for version bumps (e.g., standard-version)
-      ]
-    ],
-    "scope-case": [2, "always", "kebab-case"],
-    "subject-case": [2, "always", ["sentence-case"]],
     "type-enum": [
       2,
       "always",
       ["feat", "fix", "refactor", "style", "docs", "test", "build", "ci", "chore", "perf", "revert"]
-    ]
+    ],
+    "scope-empty": [2, "never"],
+
+    "subject-empty": [2, "never"],
+    "subject-case": [2, "always", ["lower-case"]]
   }
 };
