fb073537607dac003ec444a0c021e4b6830c3001
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-08-06 10:14:09 +0630

    build(vercel): successfully deployed to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+  ssr: {
+    noExternal: ["@polar-sh/sdk", "@polar-sh/better-auth"]
+  },
+  optimizeDeps: {
+    exclude: ["@polar-sh/sdk/webhooks.ts"]
+  }
 });
