524d951d795662697ae44c02c497e887e90a4ff3
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-08-14 07:09:35 +0630

    chore(ui): use global layout component

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { page } from "$app/state";
 
+  import { SidebarItems as CalendarSidebarItems } from "$lib/client/components/calendar";
+  import { mainSidebarState } from "$lib/client/stores/sidebar";
   import { initUserSession } from "$lib/client/stores/user-session";
+  import { cn } from "$lib/client/utils/cn";
 
   let { children } = $props();
+
+  const isOpen = $derived(mainSidebarState());
+  let pathSegment = $derived(page.url.pathname.split("/").filter(Boolean)[0]);
 
   onMount(async () => {
     await initUserSession();
   });
 </script>
 
-<div class="bg-base-200 w-full">
-  {@render children?.()}
+<div class="bg-base-200 flex w-full">
+  <!-- Sidebar -->
+  <div
+    class={cn(
+      "bg-base-200 min-h-screen overflow-y-auto transition-all duration-300",
+      $isOpen ? "w-[255px] p-2 pt-3" : "w-0"
+    )}
+  >
+    <div class={cn("transition-opacity duration-200", $isOpen ? "opacity-100" : "opacity-0")}>
+      {#if pathSegment === "calendar"}
+        <CalendarSidebarItems />
+      {/if}
+    </div>
+  </div>
+
+  <!-- Container -->
+  <div
+    class={cn(
+      "bg-base-100 my-2 h-[calc(100vh-16px)] flex-1 overflow-hidden p-3 shadow-md",
+      $isOpen ? "ml-0 rounded-l-xl" : "m-2 rounded-xl"
+    )}
+  >
+    {@render children?.()}
+  </div>
 </div>
--- src/routes/(app)/calendar/+page.svelte
+++ src/routes/(app)/calendar/+page.svelte
@@ -1,38 +1,12 @@
 <script lang="ts">
   import { Seo } from "$lib/client/components";
-  import { Navbar, SidebarItems } from "$lib/client/components/calendar";
-  import { mainSidebarState } from "$lib/client/stores/sidebar";
-  import { cn } from "$lib/client/utils/cn";
+  import { Navbar } from "$lib/client/components/calendar";
 
   let { data } = $props();
-
-  const isOpen = $derived(mainSidebarState());
 </script>
 
 <Seo seo={data.seo} />
 
-<div class="flex w-full">
-  <!-- Sidebar -->
-  <div
-    class={cn(
-      "bg-base-200 min-h-screen overflow-y-auto transition-all duration-300",
-      $isOpen ? "w-[255px] p-2 pt-3" : "w-0"
-    )}
-  >
-    <div class={cn("transition-opacity duration-200", $isOpen ? "opacity-100" : "opacity-0")}>
-      <SidebarItems />
-    </div>
-  </div>
+<Navbar />
 
-  <!-- Calendar container -->
-  <div
-    class={cn(
-      "bg-base-100 my-2 h-[calc(100vh-16px)] flex-1 overflow-hidden p-3 shadow-md",
-      $isOpen ? "ml-0 rounded-l-xl" : "m-2 rounded-xl"
-    )}
-  >
-    <Navbar />
-
-    Main calendar content
-  </div>
-</div>
+Main calendar content
