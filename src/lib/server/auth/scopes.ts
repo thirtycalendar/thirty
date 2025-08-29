67796097511adf405c19ad680eaaad1e63e260f6
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-08-29 18:03:37 +0630

    feat(core)!: remove db and auth

--- src/lib/server/auth/scopes.ts
+++
@@ -1,9 +0,0 @@
-export const googleScopes = [
-  // Basic user info
-  "openid",
-  "https://www.googleapis.com/auth/userinfo.email",
-  "https://www.googleapis.com/auth/userinfo.profile",
-
-  "https://www.googleapis.com/auth/calendar.readonly",
-  "https://www.googleapis.com/auth/calendar.events.readonly"
-];
--- src/lib/server/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-import { drizzle } from "drizzle-orm/neon-http";
-
-import { dbEnvConfig } from "../utils/env-config";
-
-const client = neon(dbEnvConfig.dbUrl);
-
-export const db = drizzle({ client });
