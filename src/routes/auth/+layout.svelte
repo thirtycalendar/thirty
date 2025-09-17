bcede1d2717c7f57a1df5104eca3585cd851d3e6
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-09-17 10:59:34 +0630

    chore: automatically hide the sidebar on mount in sm screens

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   import { ArrowLeft01Icon } from "@hugeicons/core-free-icons";
 
   import { Icon } from "$lib/client/components/icons";
+  import { mainSidebarStore } from "$lib/client/stores/boolean-store";
 
   let { children } = $props();
+
+  onMount(() => {
+    if (window.innerWidth <= 640) {
+      mainSidebarStore.set(false);
+    }
+  });
 </script>
 
 <main class="bg-base-200 min-h-screen w-full p-4">
