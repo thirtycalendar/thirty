926e815b912255aef7a685141a1ee2c0601880c2
Sithu Khant <122719409+itsithu@users.noreply.github.com>
2025-10-02 15:40:15 +0630

    chore: change default font family

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -2,7 +2,9 @@
 </script>
 
 <div class="p-2">
-  <button class="btn font-normal">Hello</button>
+  <button class="btn btn-primary font-normal">Hello</button>
+  <button class="btn btn-secondary font-normal">Hello</button>
+  <button class="btn btn-accent font-normal">Hello</button>
 
   <br />
   <br />
